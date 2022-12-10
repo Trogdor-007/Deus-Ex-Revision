0ed8ef5d455c0d6bc72052ad8568752da88adca2
Trogdor <matthewhintz007@gmail.com>
2022-12-10 13:19:22 -0500

    updated as i borke it lol
    asdfasdf

--- copy_save_game.py
+++ copy_save_game.py
@@ -1,5 +1,3 @@
-#Whenever a new folder is created within the directory "C:\Program Files (x86)\Steam\steamapps\common\Deus Ex\Revision\BiomodSave", then copy the contents of the folder into the directory C:\Program Files (x86)\Steam\steamapps\common\Deus Ex\Revision\BiomodSave\Current.
-
 import os
 import shutil
 import time
@@ -17,17 +15,22 @@ source_dir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Deus Ex\\Revisi
 destination_dir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Deus Ex\\Revision\\BiomodSave\\Current"
 
 #The list of folders in the source directory
-source_dir_list = os.listdir(source_dir)
+
 
 #Check the timestamps of the source directory creation time and if created within the last 5 seconds, then copy the contents of the folder into the destination directory. Make the for loop below run every 5 seconds.
 for i in range(17280):
     print("Script is running through the for loop to copy save games.")
-    
-    for folder in source_dir_list:        
-        if os.path.getctime(os.path.join(source_dir, folder)) > time.time() - sleep_time_in_seconds:
+    source_dir_list = os.listdir(source_dir)
+
+    for folder in source_dir_list:    
+        if os.path.getctime(os.path.join(source_dir, folder)) > time.time() - 5:
+
+            print(f"printing this thing: {os.path.getctime(os.path.join(source_dir, folder))}")
+
             #Overwrite the contents of the destination directory with the contents of the source directory
+            print(f"Copying {folder} to {destination_dir}.")
             shutil.copytree(os.path.join(source_dir, folder), destination_dir, dirs_exist_ok=True)
     
     #print script is sleeping for 5 seconds
-    print(f"Sleeping for {sleep_time_in_seconds} seconds")
-    time.sleep(sleep_time_in_seconds)
+    print(f"Sleeping for {5} seconds")
+    time.sleep(5)
